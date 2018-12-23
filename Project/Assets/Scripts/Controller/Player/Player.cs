564d8306c2d3e3d5cbb1169b2973539183a04e00
Vinicius Monteiro <vmonteirosamuel91@outlook.com>
2018-12-23 03:22:04 +0000

    NSubstitute plugin inserted for mock tests

--- Project/Assets/Scripts/Controller/Player/Player.cs
+++ Project/Assets/Scripts/Controller/Player/Player.cs
@@ -2,48 +2,53 @@
 using UnityEngine;
 using Zenject;
 
-public class Player : MonoBehaviour {
+namespace PlayerController
+{
+    public class Player : MonoBehaviour {
 
-    public float speed = 20;
-    public Movement movement;
-    public IUnityService unityService;
-    public ISoundManager soundManager;
+        public float speed = 20;
+        public Movement movement;
+        public IUnityService unityService;
+        public ISoundManager soundManager;
 
-    [Inject(Id = "PlayerWeapon")]
-    private IWeapon _weapon;
+        [Inject(Id = "PlayerWeapon")]
+        private IWeapon _weapon;
 
-    // Use this for initialization
-    void Start () {
+        // Use this for initialization
+        void Start () {
 
-        //_weapon.Charge();
+            //_weapon.Charge();
 
-        movement = new Movement(speed);
-        if (unityService == null)
-            unityService = new UnityService();
+            movement = new Movement(speed);
 
-        //if (soundManager == null)
-        //    soundManager = new SoundManager();
-	}
 
-    [Inject]
-    public void Construct(ISoundManager _soundManager)
-    {
-        soundManager = _soundManager;
-    }
-	
-	// Update is called once per frame
-	void Update () {
+            if (unityService == null)
+                unityService = new UnityService();
 
-        transform.position += movement.Calculate(
-            unityService.GetAxis("Horizontal"),
-            unityService.GetDeltaTime());
-        
-        if (unityService.GetAxis("Horizontal") != 0 &&
-            !soundManager.IsPlayingSound(GetComponent<AudioSource>()))
+            //if (soundManager == null)
+            //    soundManager = new SoundManager();
+	    }
+
+        [Inject]
+        public void Construct(ISoundManager _soundManager)
         {
-            soundManager.Play(GetComponent<AudioSource>());
+            soundManager = _soundManager;
         }
+	
+	    // Update is called once per frame
+	    void Update () {
 
-	}
+            transform.position += movement.Calculate(
+                unityService.GetAxis("Horizontal"),
+                unityService.GetDeltaTime());
+        
+            if (unityService.GetAxis("Horizontal") != 0 &&
+                !soundManager.IsPlayingSound(GetComponent<AudioSource>()))
+            {
+                soundManager.Play(GetComponent<AudioSource>());
+            }
 
+	    }
+
+    }
 }
---
+++ Project/Assets/Tests/PlayMode Test/MovementTester.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor.Build;
+using NSubstitute;
+
+namespace Tests
+{
+    public class MovementTester
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MovementTesterSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MovementTesterWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+
+            ////Arrange
+            //var player = new GameObject().AddComponent<Player>();
+            //var uService = Substitute.For<IUnityService>();
+            //player.unityService = uService;
+
+            ////Act
+            //player.speed = 1;
+            //uService.GetAxis("Horizontal").Returns(1);
+            //uService.GetDeltaTime().Returns(1);
+
+            yield return null; // skips 1 frame
+
+            ////Assert
+            //Assert.AreEqual(1, player.transform.position.x);
+
+        }
+    }
+}
